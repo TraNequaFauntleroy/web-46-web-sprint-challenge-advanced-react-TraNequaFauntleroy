961edf1f78d1494f09567d389f1682f013862338
NeNeTheCreative <shecodes@nenethecreative.com>
2021-09-03 14:40:50 -0400

    finished

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import MutationObserver from 'mutationobserver-shim';
 import { render, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event'
 
 // Write up the two tests here and make sure they are testing what the title shows
 
@@ -10,23 +11,35 @@ test("renders without errors", () => {
 });
 
 test("shows success message on submit with form details", async () => {
-    // render(<CheckoutForm/>)
-    // const firstName = screen.queryByLabelText('First Name:')
-    // userEvent.type(firstName, 'NeNeee')
-    // const lastName = screen.queryByLabelText('Last Name:')
-    // userEvent.type(lastName, 'Fauntleroy')
-    // const address = screen.queryByLabelText('Address:')
-    // userEvent.type(address, '123 Love Lane')
-    // const city = screen.queryByLabelText('City:')
-    // userEvent.type(city, 'Loveland')
-    // const state = screen.queryByLabelText('State:')
-    // userEvent.type(state, 'CA')
-    // const zip = screen.queryByLabelText('Zip')
-    // userEvent.type(zip, '08977')
-    // const submit = getByRole('button')
-    // userEvent.click(submit)
-    // await waitFor(() => {
-    //     const success = screen.getByTestId('successMessage')
-    //     expect(success).toBeInTheDocument();
-    // })
+    render(<CheckoutForm/>)
+    const firstName = screen.findByLabelText(/First Name:/i)
+    userEvent.type(firstName, 'NeNeee')
+    const lastName = screen.findByLabelText(/last Name:/i)
+    userEvent.type(lastName, 'Fauntleroy')
+    const address = screen.findByLabelText(/address:/i)
+    userEvent.type(address, '123 Love Lane')
+    const city = screen.findByLabelText(/city:/i)
+    userEvent.type(city, 'Loveland')
+    const state = screen.findByLabelText(/state:/i)
+    userEvent.type(state, 'CA')
+    const zip = screen.findByLabelText(/zip:/i)
+    userEvent.type(zip, '08977')
+    const checkout = getByRole('button')
+    userEvent.click(checkout)
+    await waitFor(() => {
+        const success = screen.getByTestId('successMessage')
+        expect(success).toBeInTheDocument();
+        const confirm = screen.findByText(/You have ordered some plants! Woo-hoo! /i)
+        expect(confirm).toBeInTheDocument();
+        const emoji = screen.getByRole('img')
+        expect(emoji).toBeInTheDocument();
+        const confirm2 =screen.getByText(/Your new green friends will be shipped to:/i)
+        expect(confirm2).toBeInTheDocument()
+        expect(firstName).toBeInTheDocument()
+        expect(lastName).toBeInTheDocument()
+        expect(address).toBeInTheDocument()
+        expect(city).toBeInTheDocument()
+        expect(state).toBeInTheDocument()
+        expect(zip).toBeInTheDocument()
+    })
 });
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,17 +1,17 @@
 // write your custom hook here to control your checkout form
 import { useState } from "react";
 
-
-
 const useForm = (initialValue) => {
-    const [values, setValues] = useState('form', initialValue)
+    const [values, setValues] = useState(initialValue)
 
     const handleChanges = (e) => {
-        setValues({ ...values, [e.target.name]: e.target.value });
+        setValues({ 
+            ...values, 
+            [e.target.name]: e.target.value });
       };
     
     
-    return [values, handleChanges ];
+    return [values, handleChanges];
 
 }
 
